607c987104b3802ece8dc8bca02870ddedcfc2d2
mkondratek <mkondratek@virtuslab.com>
2020-12-30 18:19:17 +0100

    Remove comments on resource loading

--- src/jvmMain/kotlin/com/radeusgd/trachonline/gamedefinition/GameDefinition.kt
+++ src/jvmMain/kotlin/com/radeusgd/trachonline/gamedefinition/GameDefinition.kt
@@ -56,7 +56,7 @@ data class GameDefinition(val name: String, val decks: List<Deck>) {
         fun parse(json: String): GameDefinition = Json.decodeFromString(json)
         fun loadResource(name: String): GameDefinition? {
             val text = this::class.java.classLoader.getResource(name)?.readText()
-                ?.lines()?.filter { l -> l.trim().startsWith("//") }?.joinToString()
+                ?.lines()?.filter { !it.trimStart().startsWith("//") }?.joinToString("\n")
             return text?.let { parse(it) }
         }
     }
